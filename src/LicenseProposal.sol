52c0e5f815eb0a45ceb7490a1a335a95cd3e5be5
Semi Invader <=>
2023-12-09 14:04:14 -0800

    implement v0.1

--- src/LicenseProposal.sol
+++ src/LicenseProposal.sol
@@ -2,6 +2,176 @@
 
 pragma solidity 0.8.23;
 
+// Story Protocol
+import {IIPOrg} from "@story/interfaces/ip-org/IIPOrg.sol";
+import {IRegistrationModule} from "@story/interfaces/modules/registration/IRegistrationModule.sol";
+// Internal Interfaces
 import "./interface/ILicenseProposal.sol";
 
-contract LicenseProposal is ILicenseProposal {}
+error LicenseProposal__InvalidProposalOwner();
+error LicenseProposal__InvalidAssetOwner();
+error LicenseProposal__ProposalEnded();
+
+contract LicenseProposal is ILicenseProposal {
+    //-----------------------------------------------------------------
+    // State Variables
+    //-----------------------------------------------------------------
+    mapping(uint256 => Proposal) public proposals;
+    uint256 public proposalCount;
+    IRegistrationModule public immutable REGISTRATION_MODULE;
+
+    //-----------------------------------------------------------------
+    // Constructor
+    //-----------------------------------------------------------------
+    constructor(address _registrationModule) {
+        REGISTRATION_MODULE = IRegistrationModule(_registrationModule);
+    }
+
+    //-----------------------------------------------------------------
+    // External Functions
+    //-----------------------------------------------------------------
+    function createProposal(
+        string calldata _proposal,
+        uint[] calldata _asset_ids,
+        uint _endTime
+    ) external {
+        proposalCount++;
+        uint length = _asset_ids.length;
+        ORG_ASSET_VOTE[] memory votes = new ORG_ASSET_VOTE[](length);
+        for (uint i = 0; i < length; i++) {
+            votes[i] = ORG_ASSET_VOTE.PENDING;
+        }
+        if (_endTime > block.timestamp) {
+            _endTime = block.timestamp + 30 days;
+        }
+        proposals[proposalCount] = Proposal({
+            proposalID: proposalCount,
+            endTime: _endTime,
+            creator: msg.sender,
+            proposal: _proposal,
+            asset_ids: _asset_ids,
+            votes: votes,
+            over: false,
+            accepted: false
+        });
+
+        emit CreatedProposal(proposalCount, msg.sender, _proposal, _asset_ids);
+    }
+
+    function orgAssetVote(
+        string calldata _uri,
+        uint256 _proposal,
+        uint256 index_id,
+        bool _vote
+    ) external {
+        Proposal storage proposal = proposals[_proposal];
+        if (proposal.endTime < block.timestamp || proposal.over)
+            revert LicenseProposal__ProposalEnded();
+        uint asset_id = proposal.asset_ids[index_id];
+        address owner = REGISTRATION_MODULE.ownerOf(asset_id);
+        if (owner != msg.sender) revert LicenseProposal__InvalidAssetOwner();
+        if (bytes(_uri).length > 0) {
+            emit AmmendmentAdded(_proposal, asset_id, owner, _uri);
+            proposal.proposal = _uri;
+        }
+        proposal.votes[index_id] = _vote
+            ? ORG_ASSET_VOTE.APPROVED
+            : ORG_ASSET_VOTE.DENIED;
+        emit VotedOnProposal(_proposal, asset_id, proposal.votes[index_id]);
+        //@todo if DENIED, end proposal
+        _endProposal(_proposal, _vote);
+    }
+
+    function endProposal(uint256 _proposal, bool accept_deny) external {
+        Proposal storage proposal = proposals[_proposal];
+        if (proposal.endTime > block.timestamp || proposal.over)
+            revert LicenseProposal__ProposalEnded();
+        if (proposal.creator != msg.sender)
+            revert LicenseProposal__InvalidProposalOwner();
+        for (uint i = 0; i < proposal.votes.length; i++) {
+            if (proposal.votes[i] == ORG_ASSET_VOTE.DENIED) {
+                accept_deny = false;
+                break;
+            }
+        }
+        _endProposal(_proposal, accept_deny);
+    }
+
+    //-----------------------------------------------------------------
+    // Private Functions
+    //-----------------------------------------------------------------
+    function _endProposal(uint256 _proposal, bool accept_deny) private {
+        Proposal storage proposal = proposals[_proposal];
+        proposal.accepted = accept_deny;
+        if (accept_deny) {
+            //@todo create license
+            emit ProposalAccepted(_proposal);
+        } else {
+            emit ProposalDenied(_proposal);
+        }
+        proposal.over = true;
+    }
+
+    //-----------------------------------------------------------------
+    // External VIEW Functions
+    //-----------------------------------------------------------------
+    function voteStatus(
+        uint256 _proposal
+    )
+        external
+        view
+        returns (uint[] memory asset_ids, ORG_ASSET_VOTE[] memory votes)
+    {
+        Proposal storage proposal = proposals[_proposal];
+        asset_ids = proposal.asset_ids;
+        votes = proposal.votes;
+    }
+
+    function orgAssetsProposals(
+        uint _asset_id,
+        bool _is_pending
+    ) external view returns (uint[] memory) {
+        uint length = proposalCount + 1;
+        uint[] memory asset_proposals = new uint[](length);
+        uint count = 0;
+        for (uint i = 1; i < length; i++) {
+            Proposal storage proposal = proposals[i];
+            uint index = orgAssetProposalIndex(i, _asset_id);
+            if (index == type(uint256).max) continue;
+            if (
+                !proposal.over &&
+                _is_pending &&
+                proposal.votes[index] != ORG_ASSET_VOTE.PENDING
+            ) continue;
+            asset_proposals[count] = i;
+            count++;
+        }
+    }
+
+    function orgAssetProposalIndex(
+        uint256 _proposal,
+        uint256 _asset_id
+    ) public view returns (uint256) {
+        Proposal storage proposal = proposals[_proposal];
+        uint length = proposal.asset_ids.length;
+        for (uint i = 0; i < length; i++) {
+            if (proposal.asset_ids[i] == _asset_id) {
+                return i;
+            }
+        }
+        return type(uint256).max;
+    }
+
+    function userProposals(address _user, bool isPending) external view {
+        uint length = proposalCount + 1;
+        uint[] memory user_proposals = new uint[](length);
+        uint count = 0;
+        for (uint i = 1; i < length; i++) {
+            Proposal storage proposal = proposals[i];
+            if (proposal.creator != _user) continue;
+            if (isPending && proposal.over) continue;
+            user_proposals[count] = i;
+            count++;
+        }
+    }
+}
--- src/interface/ILicenseProposal.sol
+++ src/interface/ILicenseProposal.sol
@@ -11,9 +11,12 @@ interface ILicenseProposal {
 
     struct Proposal {
         uint256 proposalID;
+        uint256 endTime;
         address creator;
+        bool over;
+        bool accepted;
         string proposal;
-        address[] assets_orgs;
+        uint[] asset_ids;
         ORG_ASSET_VOTE[] votes;
     }
 
@@ -21,12 +24,14 @@ interface ILicenseProposal {
     /**
      * @notice allows a user to create a proposal to create a license to use IP Assets / IP Orgs
      * @param _proposal An IPFS URI proposal document
-     * @param _assets_orgs An array of IP Asset / IP Org IDs
+     * @param _asset_ids An array of IP Assets
+     * @param _endTime The time at which the proposal will end
      * @dev
      */
     function createProposal(
         string calldata _proposal,
-        address[] calldata _assets_orgs
+        uint[] calldata _asset_ids,
+        uint _endTime
     ) external;
 
     /**
@@ -38,7 +43,7 @@ interface ILicenseProposal {
      * @dev
      */
     function orgAssetVote(
-        string memory uri,
+        string calldata uri,
         uint256 _proposal,
         uint256 index_id,
         bool _vote
@@ -50,12 +55,12 @@ interface ILicenseProposal {
      * @param accept_deny Status to accept or deny the proposal
      * @dev this will be called as soon as any of the assets/orgs deny the proposal
      */
-    function proposalEnd(uint256 _proposal, bool accept_deny) external;
+    function endProposal(uint256 _proposal, bool accept_deny) external;
 
     /**
      *
      * @param _proposal The proposal ID
-     * @return assets_orgs The orgs and assets that are being voted on
+     * @return asset_ids The orgs and assets that are being voted on
      * @return votes Each org/asset's vote
      */
     function voteStatus(
@@ -63,35 +68,62 @@ interface ILicenseProposal {
     )
         external
         view
-        returns (address[] memory assets_orgs, ORG_ASSET_VOTE[] memory votes);
+        returns (uint[] memory asset_ids, ORG_ASSET_VOTE[] memory votes);
 
     /**
      * @notice returns all the proposal ID's for a given org/asset
-     * @param _org_asset ]The org/asset ID
+     * @param _asset_id The asset ID
+     * @param _is_pending If true, returns all pending proposals, else returns all proposals
      * @dev to be called only by UI
      */
     function orgAssetsProposals(
-        address _org_asset
+        uint _asset_id,
+        bool _is_pending
     ) external view returns (uint256[] memory proposals);
+
+    /**
+     * @notice Return the index of an asset in a proposal asset array
+     * @param _asset_id The Id to check the index of
+     * @param _proposal The proposal to check for assets
+     * @return index The index of the asset in the proposal array
+     */
+    function orgAssetProposalIndex(
+        uint _asset_id,
+        uint256 _proposal
+    ) external view returns (uint256 index);
+
+    /**
+     * @notice fetches all IDs of proposals created by a user
+     * @param _user The user's address
+     * @param isPending If true, returns all pending proposals, else returns all proposals
+     */
+    function userProposals(
+        address _user,
+        bool isPending
+    ) external view returns (uint256[] memory proposal_ids);
 
     //-----------------------------------------------------------------
     event CreatedProposal(
         uint256 indexed proposalID,
         address indexed creator,
         string proposal,
-        address[] assets_orgs
+        uint[] asset_ids
     );
 
     event VotedOnProposal(
         uint256 indexed proposalID,
-        address indexed voter,
+        uint indexed assetIDVoted,
         ORG_ASSET_VOTE vote
     );
 
-    event ProposalEnded(
+    event ProposalAccepted(uint256 indexed proposalID);
+    event ProposalDenied(uint256 indexed proposalID);
+
+    event AmmendmentAdded(
         uint256 indexed proposalID,
+        uint256 indexed asset_id,
         address indexed creator,
-        bool accept_deny_status
+        string ammendment_uri
     );
     //-----------------------------------------------------------------
 }
